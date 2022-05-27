dcb9d0bee7d50f1fd5cfb9a7b694f343caf28cff
baroka <birotori@gmail.com>
2022-05-27 20:33:15 +0100

    gitpage deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/server.js',
+  entry: './server.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
