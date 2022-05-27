568a5e9c190bf304f86b7566bc06c1035ab3f728
baroka <birotori@gmail.com>
2022-05-27 18:46:27 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/server.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
